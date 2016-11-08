0c99ae60878015a9c67821afdb4fe4d9c4fab4c5
Jaccao <jaccao@bitbucket.org>
2016-11-08 20:33:11 +0100

    update screen

--- app/src/main/java/com/allergyiap/activities/ConfigurationActivity.java
+++ app/src/main/java/com/allergyiap/activities/ConfigurationActivity.java
@@ -2,6 +2,8 @@ package com.allergyiap.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.allergyiap.R;
 
@@ -11,5 +13,10 @@ public class ConfigurationActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configuration);
+    }
+    public void onClickOk(View v){
+        Toast teste = Toast.makeText(getApplicationContext(), "teste", Toast.LENGTH_LONG);
+        teste.show();
+
     }
 }
