487893fb45e67841394602626080ed8a419dcff7
jose <100448facens@gmail.com>
2016-11-09 09:55:06 +0100

    init development MapActivity

--- app/src/main/java/com/allergyiap/activities/MapActivity.java
+++ app/src/main/java/com/allergyiap/activities/MapActivity.java
@@ -1,14 +1,74 @@
 package com.allergyiap.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import com.allergyiap.R;
+import android.view.View;
 
-public class MapActivity extends BaseActivity {
+import com.allergyiap.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends BaseActivity implements OnMapReadyCallback, GoogleMap.OnInfoWindowClickListener, GoogleMap.InfoWindowAdapter {
+
+    static String TAG = "MapsActivity";
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        Marker marker = mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        marker.showInfoWindow();
+        mMap.setOnInfoWindowClickListener(this);
+        mMap.setInfoWindowAdapter(this);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        startActivity(new Intent(this, MapAllergyLevelsActivity.class));
+    }
+
+    /**
+     * La interfaz contiene dos métodos que puedes implementar: getInfoWindow(Marker) y getInfoContents(Marker).
+     * La API primero llamará a getInfoWindow(Marker).
+     * Si el valor devuelto es null, llamará a getInfoContents(Marker).
+     * Si el valor devuelto también es null, se usará la ventana de información predeterminada.
+     *
+     * @param marker
+     * @return
+     */
+    @Override
+    public View getInfoWindow(Marker marker) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker marker) {
+        return null;
     }
 }
