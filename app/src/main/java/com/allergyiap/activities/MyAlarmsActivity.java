97f244f5558d4c980dc6ab2b33159ec647d8c232
gore5 <carolina.romeu.farre@gmail.com>
2016-11-12 01:41:29 +0100

    You can select the allergies to notify and distribute the elements in screen

--- app/src/main/java/com/allergyiap/activities/MyAlarmsActivity.java
+++ app/src/main/java/com/allergyiap/activities/MyAlarmsActivity.java
@@ -11,11 +11,13 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -38,13 +40,13 @@ public class MyAlarmsActivity extends BaseActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                final CharSequence[] items = {" Monday "," Tuesday "," Wednesday "," Thursday "," Friday "," Saturday "," Sunday "};
+                final CharSequence[] items = {" Monday ", " Tuesday ", " Wednesday ", " Thursday ", " Friday ", " Saturday ", " Sunday "};
                 // arraylist to keep the selected items
-                final ArrayList seletedItems=new ArrayList();
+                final ArrayList seletedItems = new ArrayList();
 
-                AlertDialog.Builder builder = new AlertDialog.Builder (v.getContext(),R.style.AlertsTheme);
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext(), R.style.AlertsTheme);
                 builder.setTitle("Select the days of the week");
-                builder.setMultiChoiceItems (items, null,
+                builder.setMultiChoiceItems(items, null,
                         new DialogInterface.OnMultiChoiceClickListener() {
                             // indexSelected contains the index of item (of which checkbox checked)
                             @Override
@@ -63,40 +65,41 @@ public class MyAlarmsActivity extends BaseActivity {
                         })
                         // Set the action buttons
                         .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            String result ="";
+                            String result = "";
                             String myText;
+
                             @Override
                             public void onClick(DialogInterface dialog, int id) {
                                 Collections.sort(seletedItems);
-                                for (int i=0; i<seletedItems.size();i++) {
-                                    switch ((int)seletedItems.get(i)){
+                                for (int i = 0; i < seletedItems.size(); i++) {
+                                    switch ((int) seletedItems.get(i)) {
                                         case 0:
-                                            result+=" Mo ";
+                                            result += " Mo ";
                                             break;
                                         case 1:
-                                            result+=" Tu ";
+                                            result += " Tu ";
                                             break;
                                         case 2:
-                                            result+=" W ";
+                                            result += " W ";
                                             break;
                                         case 3:
-                                            result+=" Th ";
+                                            result += " Th ";
                                             break;
                                         case 4:
-                                            result+=" Fr ";
+                                            result += " Fr ";
                                             break;
                                         case 5:
-                                            result+=" Sa ";
+                                            result += " Sa ";
                                             break;
                                         case 6:
-                                            result+=" Su ";
-                                        break;
+                                            result += " Su ";
+                                            break;
                                         default:
-                                            result="";
+                                            result = "";
                                             break;
                                     }
                                 }
-                                button.setText( result);
+                                button.setText(result);
                             }
                         })
                         .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -119,21 +122,89 @@ public class MyAlarmsActivity extends BaseActivity {
                 Calendar mcurrentTime = Calendar.getInstance();
                 int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = mcurrentTime.get(Calendar.MINUTE);
-                mTimePicker = new TimePickerDialog(MyAlarmsActivity.this,R.style.AlertsTheme, new TimePickerDialog.OnTimeSetListener() {
+                mTimePicker = new TimePickerDialog(MyAlarmsActivity.this, R.style.AlertsTheme, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        button2.setText( selectedHour + ":" + String.format("%02d", selectedMinute));
+                        button2.setText(selectedHour + ":" + String.format("%02d", selectedMinute));
                     }
                 }, hour, minute, true);//Yes 24 hour time
                 mTimePicker.setTitle("Select Time");
-
+                mTimePicker.setButton(DialogInterface.BUTTON_POSITIVE, "OK", mTimePicker);
+                mTimePicker.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", mTimePicker);
                 mTimePicker.show();
 
             }
         });
 
+        final Button button3 = (Button) findViewById(R.id.allergy);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
+                final CharSequence[] items = {" Wall Pellitory ", " Gramineae ", " Olive tree ", " Australian pine ",
+                        " Chenopodiaceae/Amaranthaceae ", " Compositae ", " Cruciferae ",
+                        " Cypress ", " Heath ", " Mercury "," Mugwort ", " Palms ", " Alternaria ", " Cladosporium "};
+                // arraylist to keep the selected items
+                final ArrayList seletedItems = new ArrayList();
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext(), R.style.AlertsTheme);
+                builder.setTitle("Select the allaergies to notify");
+                builder.setMultiChoiceItems(items, null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            // indexSelected contains the index of item (of which checkbox checked)
+                            @Override
+                            public void onClick(DialogInterface dialog, int indexSelected,
+                                                boolean isChecked) {
+                                if (isChecked) {
+                                    // If the user checked the item, add it to the selected items
+                                    // write your code when user checked the checkbox
+                                    seletedItems.add(indexSelected);
+                                } else if (seletedItems.contains(indexSelected)) {
+                                    // Else, if the item is already in the array, remove it
+                                    // write your code when user Uchecked the checkbox
+                                    seletedItems.remove(Integer.valueOf(indexSelected));
+                                }
+                            }
+                        })
+                        // Set the action buttons
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            String result = "";
+                            String myText;
+
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                //  Your code when user clicked on Cancel
+
+                            }
+                        });
+
+                builder.create().show();//AlertDialog dialog; create like this outside onClick
+            }
+        });
     }
 
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch (view.getId()) {
+            case R.id.checkBox2:
+                if (checked)
+                {
+                    Toast.makeText(getApplicationContext(), "Alarm On", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Alarm Off", Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
+    }
 
 }
