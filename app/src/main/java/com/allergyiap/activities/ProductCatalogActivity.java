eb0364d9701f6f3a197ab7baed425e3a08aae1f1
Jaccao <jaccao@bitbucket.org>
2016-12-08 14:25:23 +0100

    map detail

--- app/src/main/java/com/allergyiap/activities/ProductCatalogActivity.java
+++ app/src/main/java/com/allergyiap/activities/ProductCatalogActivity.java
@@ -1,20 +1,12 @@
 package com.allergyiap.activities;
 
-import android.database.Cursor;
-import android.database.MatrixCursor;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.BaseColumns;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v4.widget.CursorAdapter;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 import com.allergyiap.R;
@@ -33,8 +25,6 @@ public class ProductCatalogActivity extends BaseActivity {
     List<CatalogEntity> catalogs = new ArrayList<>();
     private AsyncTask<Void, Void, Void> task;
 
-    private SimpleCursorAdapter adapterSuggestion;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,15 +36,6 @@ public class ProductCatalogActivity extends BaseActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setHasFixedSize(true);
-
-        final String[] from = new String[]{"title"};
-        final int[] to = new int[]{android.R.id.text1};
-        adapterSuggestion = new SimpleCursorAdapter(context,
-                android.R.layout.simple_list_item_1,
-                null,
-                from,
-                to,
-                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 
         createArray();
         loadData();
@@ -72,70 +53,12 @@ public class ProductCatalogActivity extends BaseActivity {
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        Log.v(TAG, "onCreateOptionsMenu");
-        //return super.onCreateOptionsMenu(menu);
-
-        getMenuInflater().inflate(R.menu.main, menu);
-
-        MenuItem searchItem = menu.findItem(R.id.menu_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setQueryHint(getString(R.string.menu_search));
-        searchView.setSuggestionsAdapter(adapterSuggestion);
-        searchView.setOnSuggestionListener(new SearchView.OnSuggestionListener() {
-            @Override
-            public boolean onSuggestionSelect(int position) {
-                showProduct(position);
-                return false;
-            }
-
-            @Override
-            public boolean onSuggestionClick(int position) {
-                showProduct(position);
-                return false;
-            }
-        });
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                if(newText.isEmpty()) {
-                    adapter.setCatalogs(catalogs);
-                } else {
-                    populateAdapter(newText);
-                }
-                return false;
-            }
-        });
-
-        return true;
-    }
-
-    private void showProduct(int position) {
-        Cursor cursor = adapterSuggestion.getCursor();
-        Integer in = cursor.getInt(position);
-        List<CatalogEntity> newList = new ArrayList<>();
-
-        for (CatalogEntity catalog : catalogs) {
-            if (catalog.id == in.intValue()) {
-                newList.add(catalog);
-            }
-        }
-
-        adapter.setCatalogs(newList);
-    }
-
     private void createArray() {
         catalogs = new ArrayList<>();
-        catalogs.add(new CatalogEntity(1, "Nasal Steroids", "These are drugs you spray into your nose", "http://www.krishnaherbals.com/images/allergy-care-zoom.jpg"));
-        catalogs.add(new CatalogEntity(2, "Antihistamines", "These drugs work against the chemical histamine", "http://www.alwaysayurveda.com/wp-content/uploads/2013/05/Arjuna1.png"));
-        catalogs.add(new CatalogEntity(3, "Decongestants", "These drugs unclog your stuffy nose", "http://www.anytimeherbal.com/images/arjuna.gif"));
-        catalogs.add(new CatalogEntity(4, "Anthipollen", "These drugs unclog your stuffy nose", "http://www.anytimeherbal.com/images/arjuna.gif"));
+        catalogs.add(new CatalogEntity(1,"Nasal Steroids","These are drugs you spray into your nose","http://www.krishnaherbals.com/images/allergy-care-zoom.jpg"));
+        catalogs.add(new CatalogEntity(1,"Antihistamines","These drugs work against the chemical histamine","http://www.alwaysayurveda.com/wp-content/uploads/2013/05/Arjuna1.png"));
+        catalogs.add(new CatalogEntity(1,"Decongestants","These drugs unclog your stuffy nose","http://www.anytimeherbal.com/images/arjuna.gif"));
+        catalogs.add(new CatalogEntity(1,"Anthipollen","These drugs unclog your stuffy nose","http://www.anytimeherbal.com/images/arjuna.gif"));
     }
 
     private void loadData() {
@@ -156,6 +79,8 @@ public class ProductCatalogActivity extends BaseActivity {
 
             @Override
             public void onItemClick(View view, int position, CatalogEntity catalogEntity) {
+                //setContentView(R.layout.product_info);
+                startActivity(new Intent(context, ProductCatalogMapActivity.class));
 
             }
         });
@@ -180,17 +105,5 @@ public class ProductCatalogActivity extends BaseActivity {
             findViewById(R.id.progress_bar).setVisibility(View.GONE);
             loadAdapter(catalogs);
         }
-    }
-
-    private void populateAdapter(String query) {
-        final MatrixCursor c = new MatrixCursor(new String[]{BaseColumns._ID, "title"});
-
-        for (CatalogEntity catalog : catalogs) {
-
-            if (catalog.title.toLowerCase().startsWith(query.toLowerCase()))
-                c.addRow(new Object[]{catalog.id, catalog.title});
-        }
-
-        adapterSuggestion.changeCursor(c);
     }
 }
